2359725773f690f464e635a0cdcbd78d9b98d251
AssetKussainov <asetius2008@gmail.com>
2020-03-29 17:29:17 +0600

    Создан тест модификации формы группы

--- fixture/group.py
+++ fixture/group.py
@@ -15,25 +15,48 @@ class GroupHelper:
         self.open_group_page()
         # init group creation
         self.driver.find_element(selenium.webdriver.common.by.By.NAME, "new").click()
-        # fill group form
-        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "group_name").click()
-        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "group_name").send_keys(group.name)
-        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "group_header").click()
-        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "group_header").send_keys(group.header)
-        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "group_footer").click()
-        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit
         self.driver.find_element(selenium.webdriver.common.by.By.NAME, "submit").click()
         self.return_to_groups()
 
+    def fill_group_form(self, group):
+        self.driver = self.app.driver
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        self.driver = self.app.driver
+        if text is not None:
+            self.driver.find_element(selenium.webdriver.common.by.By.NAME, field_name).click()
+            self.driver.find_element(selenium.webdriver.common.by.By.NAME, field_name).clear()
+            self.driver.find_element(selenium.webdriver.common.by.By.NAME, field_name).send_keys(text)
+
     def delete_first_group(self):
         self.driver = self.app.driver
         self.open_group_page()
-        # select the first group
-        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "selected[]").click()
+        self.select_first_group()
         # submit deletion
         self.driver.find_element(selenium.webdriver.common.by.By.NAME, "delete").click()
         self.return_to_groups()
+
+    def select_first_group(self):
+        self.driver = self.app.driver
+        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "selected[]").click()
+
+    def modify_first_group(self, new_group_data):
+        self.driver = self.app.driver
+        self.open_group_page()
+        self.select_first_group()
+        # open modification form
+        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "edit").click()
+        # fill group form
+        self.fill_group_form(new_group_data)
+        # submit modification
+        self.driver.find_element(selenium.webdriver.common.by.By.NAME, "update").click()
+        self.return_to_groups()
+
 
     def return_to_groups(self):
         self.driver = self.app.driver
