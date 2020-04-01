0132464716e5616bada99ca54f0b11708aeeb9b4
AssetKussainov <asetius2008@gmail.com>
2020-04-01 18:24:37 +0600

    Добавлена рандомизация удаления и модификации группы

--- fixture/session.py
+++ fixture/session.py
@@ -9,7 +9,7 @@ class SessionHelper:
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input:nth-child(7)").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
@@ -21,7 +21,11 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text[1:-1]
 
     def ensure_logout(self):
         wd = self.app.wd
