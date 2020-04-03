fea49bffec59544cd7475657fbbd4451d8264baf
AssetKussainov <asetius2008@gmail.com>
2020-04-03 22:26:04 +0600

    Вывод в json файл

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,11 @@
 from model.group import Group
 
 
-def test_add_group(app):
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
-    group = Group(name="Clients", header="Clients", footer="Clients")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="Clients", header="Clients", footer="Clients")
-#     app.group.create(group)
-#     assert len(old_groups) + 1 == app.group.count()
-#     new_groups = app.group.get_group_list()
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
